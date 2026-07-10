fcb2752781a20dfe91ee2e7bf9b8a6a97671275b
Junaili <Junaili@users.noreply.github.com>
2026-07-10 01:06:38 +0000

    deploy: 33778f0c68344d35c86cfe56608a9461b0b552ed

--- assets/esm-BBY_CUvq.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CZ_GD5lG.js","assets/index-j8D_ni67.js","assets/index-BQwrsQBZ.css","assets/dist-Bf9zDzdJ.js"])))=>i.map(i=>d[i]);
-import{t as e}from"./index-j8D_ni67.js";import{registerPlugin as t}from"./dist-Bf9zDzdJ.js";var n=t(`SignInWithApple`,{web:()=>e(()=>import(`./web-CZ_GD5lG.js`).then(e=>new e.SignInWithAppleWeb),__vite__mapDeps([0,1,2,3]))});export{n as SignInWithApple};
