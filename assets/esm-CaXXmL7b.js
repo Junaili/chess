2e813118530d9d332883e94202d100b13de49b04
Junaili <Junaili@users.noreply.github.com>
2026-07-13 02:25:51 +0000

    deploy: f728a7490e5a3cf42a6f84dadba160c7e803f048

---
+++ assets/esm-CaXXmL7b.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-PimYtCV4.js","assets/index-Bk6vno7s.js","assets/index-Du70RDca.css","assets/dist-Bf9zDzdJ.js"])))=>i.map(i=>d[i]);
+import{t as e}from"./index-Bk6vno7s.js";import{registerPlugin as t}from"./dist-Bf9zDzdJ.js";var n=t(`SignInWithApple`,{web:()=>e(()=>import(`./web-PimYtCV4.js`).then(e=>new e.SignInWithAppleWeb),__vite__mapDeps([0,1,2,3]))});export{n as SignInWithApple};
