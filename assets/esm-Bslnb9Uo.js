211657ef7827abc54998c4e32681386a0128d9b0
liechl <liechl@users.noreply.github.com>
2026-07-31 06:10:53 +0000

    deploy: b1c1bae02a51b6cda61376e05c6f7db572a9a473

---
+++ assets/esm-Bslnb9Uo.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B5oit2Fw.js","assets/index-UecZCnZX.js","assets/chunk-CMxvf4Kt.js","assets/index-D0qmcR2P.css"])))=>i.map(i=>d[i]);
+import{D as e,E as t,F as n}from"./index-UecZCnZX.js";e();var r=n(`Browser`,{web:()=>t(()=>import(`./web-B5oit2Fw.js`).then(e=>new e.BrowserWeb),__vite__mapDeps([0,1,2,3]))});export{r as Browser};
