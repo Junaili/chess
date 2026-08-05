78718cb47c1da88ebed3517b72bd75c1f32cc371
Junaili <Junaili@users.noreply.github.com>
2026-08-05 22:05:00 +0000

    deploy: 30520b6a7f37736bc12f257bade273a16bfa69b7

--- assets/esm-Dx4ihKxy.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DifYTzWA.js","assets/index-CEdy9AzY.js","assets/chunk-CMxvf4Kt.js","assets/index-D0qmcR2P.css"])))=>i.map(i=>d[i]);
-import{D as e,E as t,F as n}from"./index-CEdy9AzY.js";e();var r=n(`Browser`,{web:()=>t(()=>import(`./web-DifYTzWA.js`).then(e=>new e.BrowserWeb),__vite__mapDeps([0,1,2,3]))});export{r as Browser};
